bceece826aaae6b863a7a33e92314900ac24e511
amarjitmult <amarjits@gmail.com>
2020-04-17 22:14:02 -0700

    Updates

---
+++ precache-manifest.1efe9bf2160226d285435a0a8cc2847f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855a064048c731f6f6d894cc29f0616d",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "8ae165faee1513b1b55a",
+    "url": "/recipes/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "84831cae2f3e7b32592d",
+    "url": "/recipes/static/css/main.ea14257e.chunk.css"
+  },
+  {
+    "revision": "8ae165faee1513b1b55a",
+    "url": "/recipes/static/js/2.363b846e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.363b846e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84831cae2f3e7b32592d",
+    "url": "/recipes/static/js/main.d6089578.chunk.js"
+  },
+  {
+    "revision": "f97aae37dd215767239a",
+    "url": "/recipes/static/js/runtime-main.5bba7e5f.js"
+  },
+  {
+    "revision": "ad3bd94e518bb4be7fa240b300e2ffd8",
+    "url": "/recipes/static/media/gajar.ad3bd94e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
