38f23d35c0b38981a458b5fb107e12c176675bd4
Pinki Singh <singhpinki033@gmail.com>
2020-08-26 14:36:12 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-
-//import Recipes from'./Recipereact'
+import Recipes from './Recipereact'
 function App() {
  
   return (
     <div className="App">
-      
+     <Recipes />
    
     </div>
   );
